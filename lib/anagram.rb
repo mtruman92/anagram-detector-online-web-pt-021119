05649a4f7d3a2bd1313230bcab8b7adbb4d626c2
Mickae Truman <mtruman@nyit.edu>
2019-03-15 12:40:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :anagrams
+  
+  def initialize(anagrams)
+    @anagrams = anagrams
+  end
+  
+  def match(array)
+    array.select do |anagram|
+      anagram.split("").sort == @anagram.split("").sort
+    end
+    
+  end
+end
+  
+    
+  
