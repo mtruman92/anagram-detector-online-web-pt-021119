2356b8ef41231098a1fbf5b1a89e26edf5ac2593
Mickae Truman <mtruman@nyit.edu>
2019-03-15 13:02:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagram)
+    anagram.select do |some_word| 
+      (@word.split("").sort) == (some_word.split("").sort)
+    end    
+  end
+
+end
+  
+    
+  
